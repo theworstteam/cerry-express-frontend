5fec3da65defcab38c5831c4e4b902a22888fd03
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2019-06-23 13:02:17 +0700

    format document

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {ParcelProvider} from './context'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ParcelProvider } from "./context";
 
 ReactDOM.render(
-    <ParcelProvider>
-        <Router>
-            <App />
-        </Router>
-    </ParcelProvider>
-    , document.getElementById('root'));
+	<ParcelProvider>
+		<Router>
+			<App />
+		</Router>
+	</ParcelProvider>,
+	document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
